580e92df530cf6b782abee80290f182b085d3de7
pawel <pawtul@gmail.com>
2024-12-14 20:28:57 +0100

    Add telescope display settings

--- init.lua
+++ init.lua
@@ -166,9 +166,17 @@ require('telescope').setup({
   pickers = {
     grep_string = {
       theme = "dropdown",
+      layout_config={width=0.8}
     },
     live_grep = {
       theme = "dropdown",
+      layout_config={width=0.8}
+    },
+    find_files = {
+      layout_config={width=0.8}
+    },
+    buffers = {
+      layout_config={width=0.8}
     },
   }
 })
@@ -176,6 +184,7 @@ local telescope_builtin = require('telescope.builtin')
 vim.keymap.set('n', '<C-p>', telescope_builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', telescope_builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>ff', telescope_builtin.grep_string, { desc = 'Telescope grep string under cursor' })
+vim.keymap.set('n', '<leader>fb', telescope_builtin.buffers, { desc = 'Telescope buffers' })
 
 -- jedi-vim
 vim.g['jedi#usages_command'] = '<leader>u'
